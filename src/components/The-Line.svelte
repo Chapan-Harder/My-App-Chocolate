71ab5f9c5cba9f68f59f80e5ea7aeaca00a55b9b
Mohsen Heidari <chapanh@iran.ir>
2024-01-26 15:48:56 +0330

    01

--- src/components/The-Line.svelte
+++ src/components/The-Line.svelte
@@ -1,2 +1,6 @@
+<script lang="ts">
+	export let hidden: boolean;
+</script>
+
 <!-- The Line -->
-<div class="bg-gradient-to-r from-slate-500/0 via-[#dcb9cd] to-slate-500/0 h-[2px]"></div>
+<div {hidden} class="bg-gradient-to-r from-slate-500/0 via-[#dcb9cd] to-slate-500/0 h-[2px]"></div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,25 @@
 <script>
 	import '../app.css';
+	import Navbar from '../components/Navbar.svelte';
+	import Footer from '../components/Footer.svelte';
+	import TheLine from '../components/The-Line.svelte';
+	import { page } from '$app/stores';
 </script>
 
-<slot />
+<section class="bg-[#152235] min-h-screen grid p-11">
+	<div
+		class="bg-[#2a3a4f] rounded-tr-3xl rounded-bl-3xl px-10 pt-6 border-solid border-[3px] border-[#3b4f6b] lg:px-24 mx-0 lg:mx-20"
+	>
+		<Navbar />
+
+		<!-- Mian-App Satrt -->
+		<main>
+			<slot />
+		</main>
+
+		<!-- Mian-App Satrt -->
+
+		<TheLine hidden={$page.url.pathname == '/'} />
+		<Footer />
+	</div>
+</section>
