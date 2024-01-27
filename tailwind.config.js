e9701edf0264b3840b46e6c2b5652153ec543f5b
Mohsen Heidari <chapanh@iran.ir>
2024-01-27 12:02:05 +0330

    03.01

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,6 @@ export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {
-      margin: {
-        'calc-50': 'calc(50% - 2rem)',
-      },
       fontFamily: {
         'sans': ['FreeSans', 'sans-serif'],
         'serif': ['Noto Serif', 'serif'],
