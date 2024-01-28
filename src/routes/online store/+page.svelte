5c76e52bab7b4d0436dbb1c1c4ab94a3b869f38b
Mohsen Heidari <chapanh@iran.ir>
2024-01-28 10:05:13 +0330

    04

--- src/routes/online store/+page.svelte
+++ src/routes/online store/+page.svelte
@@ -2,6 +2,6 @@
 	import BoxShow from '../../components/Box-Show.svelte';
 </script>
 
-<section class="h-[658px] mt-12 overflow-auto">
+<section class="h-[590px] mt-12 overflow-auto md:h-[658px]">
 	<BoxShow />
 </section>
